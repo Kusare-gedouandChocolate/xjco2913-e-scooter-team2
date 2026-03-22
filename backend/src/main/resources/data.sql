7970b32f0e53a866ff5791115a0c7ec42b283172
vijeghk <1802816610@qq.com>
2026-03-23 06:53:53 +0800

    # Merge 1

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS scooter CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
-                       user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                       user_id UUID PRIMARY KEY,
                        email VARCHAR(255) NOT NULL UNIQUE,
                        password_hash VARCHAR(255) NOT NULL,
                        role VARCHAR(50) NOT NULL
